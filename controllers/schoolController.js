1e4e14c55d8339876ca6f9fdfc609134756802f4
Rohit Yadav <rohit@gmail.com>
2026-04-08 13:16:36 +0530

    Initial commit - School API

---
+++ controllers/schoolController.js
@@ -0,0 +1,77 @@
+const db = require("../config/db");
+
+
+exports.addSchool = (req, res) => {
+  const { name, address, latitude, longitude } = req.body;
+
+
+  if (!name || !address || !latitude || !longitude) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const query =
+    "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)";
+
+  db.query(query, [name, address, latitude, longitude], (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+
+    res.status(201).json({
+      message: "School added successfully",
+      schoolId: result.insertId,
+    });
+  });
+};
+
+
+exports.listSchools = (req, res) => {
+  const { latitude, longitude } = req.query;
+
+  if (!latitude || !longitude) {
+    return res.status(400).json({ message: "Latitude & Longitude required" });
+  }
+
+  const userLat = parseFloat(latitude);
+  const userLon = parseFloat(longitude);
+
+  const query = "SELECT * FROM schools";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+
+    
+    const getDistance = (lat1, lon1, lat2, lon2) => {
+      const R = 6371; // km
+      const dLat = (lat2 - lat1) * (Math.PI / 180);
+      const dLon = (lon2 - lon1) * (Math.PI / 180);
+
+      const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) *
+          Math.cos(lat2 * (Math.PI / 180)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
+
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      return R * c;
+    };
+
+    const sortedSchools = results
+      .map((school) => {
+        const distance = getDistance(
+          userLat,
+          userLon,
+          school.latitude,
+          school.longitude
+        );
+
+        return { ...school, distance };
+      })
+      .sort((a, b) => a.distance - b.distance);
+
+    res.json(sortedSchools);
+  });
+};
